7c6a71f50441ed9602c9f28a5fee01bbb1304a7a
Brittany Clark <clark.brittany.j@gmail.com>
2020-03-10 12:55:45 -0400

    html get requests set up

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,23 @@
-// Requiring path to so we can use relative routes to our HTML files
-// var path = require("path");
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
 // var db = require("../models"); should automatically recognize public and find the right files
 
 //routing
+// HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+// ---------------------------------------------------------------------------
+
 module.exports = function(app) {
   app.get("/notes", function(req, res) {
-    console.log("notes page");
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-  // res.render("notes");
-  //     res.sendFile(path.join(__dirname, "../public/login.html"));
-};
-//
-//   // Here we've add our isAuthenticated middleware to this route.
-//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-//   app.get("/members", isAuthenticated, function(req, res) {
-//     res.render("index");
-//   });
 
-//   app.get("/saved", isAuthenticated, function(req, res) {
-//     console.log("start data");
-//     db.Listings.findAll({ raw: true }).then(function(data) {
-//       console.log(data);
-//       res.render("saved", {
-//         saved: data
-//       });
-//     });
-//   });
-// };
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
